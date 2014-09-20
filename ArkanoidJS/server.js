1c941bb594c59e340dd5e52dfe3d10f25b0623ac
stiasta <stian_standahl@hotmail.com>
2014-09-20 15:34:18 +0200

    init

---
+++ ArkanoidJS/server.js
@@ -0,0 +1,17 @@
+﻿var http = require('http');
+var fs = require('fs');
+
+var port = process.env.port || 1337;
+http.createServer(function (req, res) {
+    fs.readFile("client/index.html", function (error, contents) {
+        if (error) {
+            res.writeHead(500);
+            res.end();
+        }
+
+        res.setHeader("Contet-Length", contents.length);
+        res.setHeader("Content-Type", "text/html");
+        res.statusCode = 200;
+        res.end(contents);
+    });
+}).listen(port);
